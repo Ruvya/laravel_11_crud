6d21458cb4663be8bff71d4cbbdb3d2a634cafa1
jsp6a32 <jsp6a32@scs.local>
2025-04-25 13:34:12 +0800

    You're commit message here

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,17 @@
+<div>
+    <!-- The whole future lies in uncertainty: live immediately. - Seneca -->
+</div>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width-device-width, initial-scale=1">
+        <title>Document</title>
+
+</head>
+<body>
+    <formt
+
+</body>
+</html>
