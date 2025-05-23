758f8d7e6b830f23f042a6cbbf30bf1bb16bad4f
Ruvya <ruvyannlacaba141@gmail.com>
2025-05-23 16:29:34 +0800

    update route

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,28 @@
 <?php
-use Illuminate\Support\Facades\Route;
 
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
-Route::get('/', function () {
+Route::get('/welcome', function () {
  return view('welcome');
 });
 Route::resource('products', ProductController::class);
+
+
+
+use App\Http\Controllers\AuthController;
+
+
+
+// Auth Routes
+Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register');
+Route::post('/register', [AuthController::class, 'register'])->name('register.store');
+
+Route::get('/', [AuthController::class, 'showLoginForm'])->name('login');
+Route::post('/', [App\Http\Controllers\AuthController::class, 'login'])->name('login.store');
+
+Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+// Protected route
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware('auth')->name('dashboard');
